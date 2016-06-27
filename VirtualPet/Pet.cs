519e683a87cd1bfe1f173604cc5e7329ab4c67c8
cierrajw <cw1572@nyu.edu>
2016-06-27 13:29:35 -0400

    Creating methods and properties

---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace VirtualPet
+{
+    class Pet
+    {
+        Random random = new Random();
+        private string name { get; set; }
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
+        public int Boredom { get; set; }
+        public int Sleepiness { get; set; }
+        public int Health { get; set; }
+
+        public Pet(string Name)
+        {
+            Name = "LLAMAGACHI";
+            Hunger = 0;
+            Thirst = 0;
+            Boredom = 0;
+            Health = 0;
+            Sleepiness = 0;
+        }
+
+        public int Tick()
+        {
+            int tick = random.Next(1, 10);
+            return tick;
+        }
+
+        public void Time()
+        {
+            Thread.Sleep(500);
+        }
+
+        public void Stats()
+        {
+            if (Hunger < 0)
+            {
+                Hunger = 0;
+                Console.WriteLine("{0};s hunger level is {1}", name, Hunger);
+                Hunger += Tick();
+            }
+            else if (Thirst < 0)
+            {
+                Console.WriteLine("{0};s thirst level is {1}", name, Thirst);
+                Thirst += Tick();
+            }
+            else if (Boredom < 0)
+            {
+                Console.WriteLine("{0};s boredom level is {1}", name, Boredom);
+                Boredom += Tick();
+            }
+            else if (Sleepiness < 0)
+            {
+                Console.WriteLine("{0};s sleepiness level is {1}", name, Sleepiness);
+                Sleepiness += Tick();
+            }
+            else if (Health < 0)
+            {
+                Console.WriteLine("{0};s health level is {1}", name, Health);
+                Health += Tick();
+            }
+        }
+
+        public void FeedPet()
+        {
+            Hunger += Tick();
+            Stats();
+            Console.WriteLine("You have fed your pet. Your pet's hunger is now {0}", Hunger);
+        }
+        public void GiveDrink()
+        {
+
+        }
+        public void Play()
+        {
+
+        }
+        public void PutToSleep()
+        {
+
+        }
+        public void GiveMedicine()
+        {
+
+        }
+    }
+}
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                 V");
+            Console.WriteLine("                /'>>>   Hello, I am your LLAMAGACHI! What would you like to do?!");
+            Console.WriteLine("               /*/");
+            Console.WriteLine("              / /");
+            Console.WriteLine("             /*/");
+            Console.WriteLine("            / /");
+            Console.WriteLine("    -------/*/");
+            Console.WriteLine(" --/  *  * */");
+            Console.WriteLine("  /* * *  */");
+            Console.WriteLine("  -  --- -/");
+            Console.WriteLine("   H    H");
+            Console.WriteLine("   H    H");
+            Console.WriteLine("   --   --");
+
+
+            Console.WriteLine("1. Feed me!");
+            Console.WriteLine("2. Give me a drink!");
+            Console.WriteLine("3. Play with me!");
+            Console.WriteLine("4. Put me to sleep!");
+            Console.WriteLine("5. Give me some medicine!");
+            Console.WriteLine("6. Exit");
+
+            //Creating objects
+            Pet feed = new Pet("");
+            Pet drink = new Pet("");
+            Pet play = new Pet("");
+            Pet sleep = new Pet("");
+            Pet medicine = new Pet("");
+
+            int choice = Convert.ToInt32(Console.ReadLine()); 
+
+            if(choice == 6)
+            {
+                //
+            }
+            else if(choice == 1)
+            {
+                feed.FeedPet();
+            }
+            else if(choice == 2)
+            {
+                //drink.GiveDrink();
+            }
+            else if(choice == 3)
+            {
+                //play.Play();
+            }
+            else if(choice == 4)
+            {
+                //sleep.PutToSleep();
+            }
+            else if(choice == 5)
+            {
+                //medicine.GiveMedicine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
